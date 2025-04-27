775ab7399d399cb6614ecba03ebf569882d47787
exerussus <solovyov.production@gmail.com>
2025-04-27 20:42:10 +0300

    Auto updated to version 1.0.10

--- Server/Models/ConnectionContext.cs
+++ Server/Models/ConnectionContext.cs
@@ -5,6 +5,7 @@ namespace Source.Features.GameAuthentication.Server.Models
 {
     public class ConnectionContext
     {
+        public string NickName { get; set; }
         public NetworkConnection NetworkConnection { get; set; }
         public Authenticator Authenticator { get; set; }
         public bool IsAuthenticated { get; set; }
