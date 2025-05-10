eb71282944dda066dfae9fe17438e085b0faf562
exerussus <solovyov.production@gmail.com>
2025-05-10 21:01:06 +0300

    Auto updated to version 1.0.11

--- Server/Models/ConnectionContext.cs
+++ Server/Models/ConnectionContext.cs
@@ -12,5 +12,31 @@ namespace Source.Features.GameAuthentication.Server.Models
         public bool DataApproved { get; set; }
         public float KickTime { get; set; }
         public object Data;
+
+        public static implicit operator NetworkConnection(ConnectionContext context) => context.NetworkConnection;
+        public static implicit operator Authenticator(ConnectionContext context) => context.Authenticator;
+    }
+
+    public static class ConnectionContextExtensions
+    {
+        public static void SetData<T>(this ConnectionContext context, T data) => context.Data = data;
+        public static T GetData<T>(this ConnectionContext context) => (T)context.Data;
+        public static bool TryGetData<T>(this ConnectionContext context, out T data)
+        {
+            if (context.Data == null)
+            {
+                data = default;
+                return false;
+            }
+
+            if (context.Data is not T castData)
+            {
+                data = default;
+                return false;
+            }
+
+            data = castData;
+            return true;
+        }
     }
 }
